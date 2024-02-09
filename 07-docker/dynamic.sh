f100354ae00a085cfedab2649b6d8f9d7ccc4e9a
moto_default_motive <mihail.stepushin@gmail.com>
2024-02-09 14:10:30 +0300

    Reworked static.sh

--- 07-docker/dynamic.sh
+++ 07-docker/dynamic.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -f Dockerfile -t hometask-image .
-docker run -p 8080:80 --name dynamic -it hometask-image 
+docker run -p 8080:80 --name dynamic -idt hometask-image 
 
--- 07-docker/static.sh
+++ 07-docker/static.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-docker create --name static -p 8081:80 hometask-image 
-docker cp files/index.html static:/var/www/html
-docker start static
-docker exec static a2dissite dynamic.conf
-docker exec static service apache2 reload
+docker run -p 8081:80 --name static -idt hometask-image sh -c 'a2dissite dynamic.conf;service apache2 reload;/usr/sbin/apache2ctl -DFOREGROUND'
