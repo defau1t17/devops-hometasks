f46e782129a782f34597c842258e06795db2cece
DefaultMichael <61115375+defau1t17@users.noreply.github.com>
2024-03-11 00:19:12 +0300

    Add 13-terraform/publish.tf

---
+++ 13-terraform/publish.tf
@@ -0,0 +1,8 @@
+resource "github_repository_file" "upload" {
+  for_each            = {file1 = "config.tf", file2 = "vars.tf", file3 = "wordcloud.tf", file4 = "publish.tf"}
+  repository          = var.github-repository
+  file                = "13-terraform/${each.value}"
+  content             = file("${each.value}")
+  branch              = var.github-branch
+  overwrite_on_create = true
+}
